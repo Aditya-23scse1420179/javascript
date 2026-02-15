0bd543fba348955ae6b8c723e32f8065639d7e86
Aditya-23scse1420179 <aditya.23scse1420179@galgotiasuniversity.ac.in>
2026-02-15 23:45:06 +0530

    Api

--- src/chapter6.js
+++ src/chapter6.js
@@ -1,9 +1,20 @@
 //API fetching data from an API and displaying it on the webpage
-const url="https://cat-fact.herokuapp.com";
+const url="https://dog.ceo/api/breeds/image/random";
+const factPara=document.querySelector("#fact");
+const button=document.querySelector("#btn");
+
 
 // to get data use fetch
 const getFacts=async()=>{
     console.log("getting data.....");
     let response=await fetch(url);
-    console.log(response);
-}
+    console.log(response);//json format
+    let data=await response.json();//to convert json format to js object to make it readable
+    factPara.innerText=data.message;
+};
+//ajax fetching request from api
+button.addEventListener("click",getFacts);
+// status start with 2 means success
+// status start with 4 means client error
+// status start with 5 means server error
+
